e18b43b68ee0a5b752c0f7ebc83da1840d64457a
davidsuelia <58593791+davidsuelia@users.noreply.github.com>
2020-11-21 03:49:35 +0700

    Update lobby.go

--- game/lobby.go
+++ game/lobby.go
@@ -503,7 +503,6 @@ func endGame(lobby *Lobby) {
 // over.
 func selectNextDrawer(lobby *Lobby) (*Player, bool) {
 	for index, otherPlayer := range lobby.Players {
-		if otherPlayer == lobby.Drawer {
 			//If we have someone that's drawing, take the next one
 			for i := index + 1; i < len(lobby.Players); i++ {
 				player := lobby.Players[i]
@@ -511,7 +510,6 @@ func selectNextDrawer(lobby *Lobby) (*Player, bool) {
 					return player, false
 				}
 			}
-		}
 	}
 
 	return lobby.Players[0], true
