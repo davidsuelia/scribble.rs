fe0ef1742508fabe7930a26058e1a917b60bdf59
davidsuelia <58593791+davidsuelia@users.noreply.github.com>
2020-11-21 03:54:49 +0700

    Update lobby.go

--- game/lobby.go
+++ game/lobby.go
@@ -501,15 +501,15 @@ func endGame(lobby *Lobby) {
 // doesn't tell the lobby yet. The boolean signals whether the current round is
 // over.
 func selectNextDrawer(lobby *Lobby) (*Player, bool) {
-	for index, otherPlayer := range lobby.Players {
+	for index, _ := range lobby.Players {
 			//If we have someone that's drawing, take the next one
 			for i := index + 1; i < len(lobby.Players); i++ {
 				player := lobby.Players[i]
 				if player.Connected && (strings.Contains(player.Name, "GAMBAR")){
 					return player, false
 				}
-			}
-	}
+				}
+				}
 
 	return lobby.Players[0], true
 }
